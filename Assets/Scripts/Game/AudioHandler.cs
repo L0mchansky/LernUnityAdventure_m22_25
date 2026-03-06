9c912dc4ec93cf54b4acb87363d1f24b28290531
L0mchansky <persmax13@gmail.com>
2026-03-06 23:43:49 +0300

    refactor

---
+++ Assets/Scripts/Game/AudioHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine.Audio;
+
+namespace LernUnityAdventure_m24_25
+{
+    public class AudioHandler
+    {
+        private const float OffVolumeValue = -80;
+        private const float OnVolumeValue = 0;
+
+        private const string MainMelodyKey = "MainMelodyVolume";
+        private const string EffectsVolumeKey = "EffectsVolume";
+
+        private readonly AudioMixer _audioMixer;
+        public AudioHandler(AudioMixer audioMixer)
+        {
+            _audioMixer = audioMixer;
+        }
+
+        public bool IsMusicOn() => IsVolumeOn(MainMelodyKey);
+        public bool IsEffectsOn() => IsVolumeOn(EffectsVolumeKey);
+
+        public void OffMusic() => OffVolume(MainMelodyKey);
+        public void OnMusic() => OnVolume(MainMelodyKey);
+
+        public void OffEffects() => OffVolume(EffectsVolumeKey);
+        public void OnEffects() => OnVolume(EffectsVolumeKey);
+
+        private bool IsVolumeOn(string key)
+            => _audioMixer.GetFloat(key, out float volume) && Math.Abs(volume - OnVolumeValue) <= 0.01f;
+
+        private void OnVolume(string key) => _audioMixer.SetFloat(key, OnVolumeValue);
+
+        private void OffVolume(string key) => _audioMixer.SetFloat(key, OffVolumeValue);
+    }
+}
--- Assets/Scripts/Game/GameAudio.cs
+++ Assets/Scripts/Game/GameAudio.cs
@@ -1,14 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 namespace LernUnityAdventure_m24_25
 {
     public class GameAudio: MonoBehaviour
     {
         [SerializeField] private AudioSource _mainMelody;
+        [SerializeField] private AudioMixer _audioMixer;
+
+        private AudioHandler _audioHandler;
+
+        private void Awake()
+        {
+            _audioHandler = new AudioHandler(_audioMixer);
+        }
 
         public void Start()
         {
             _mainMelody.Play();
+        }
+
+        public void ToggleMusic()
+        {
+            if (_audioHandler.IsMusicOn())
+            {
+                _mainMelody.Stop();
+                _audioHandler.OffMusic();
+            }
+            else
+            {
+                _mainMelody.Play();
+                _audioHandler.OnMusic();
+            }
+        }
+
+        public void ToggleEffects()
+        {
+            if (_audioHandler.IsEffectsOn())
+            {
+                _audioHandler.OffEffects();
+            }
+            else
+            {
+                _audioHandler.OnEffects();
+            }
         }
     }
 }
